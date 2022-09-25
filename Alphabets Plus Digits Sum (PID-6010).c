c82d923f56195fbc885cfd3eedce2756d6acb6bf
DB_046_UNFOUNDER <85393004+Anbu0646@users.noreply.github.com>
2022-09-25 20:07:45 +0530

    Update and rename Alphabets Plus Digits Sum(PID-6010).txt to Alphabets Plus Digits Sum (PID-6010).c

--- Alphabets Plus Digits Sum (PID-6010).c
+++ Alphabets Plus Digits Sum (PID-6010).c
@@ -1,21 +1,3 @@
-Python:
-
-S=input().strip()
-c=0
-a='abcdefghijklmnopqrstuvwxyz'
-
-for i in S:
-    if(i.isdigit()):
-        c+=int(i)
-
-for i in S:
-    if(i.isalpha()):
-        i=i.lower()
-        print(a[(a.index(i)+c)%26],end="")
-
-
-
-C:
 
 #include<stdio.h>
 #include<stdlib.h>
