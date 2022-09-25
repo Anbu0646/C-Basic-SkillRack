257febbaf0c1b00a0b3ba0bba3e068d992f2793c
DB_046_UNFOUNDER <85393004+Anbu0646@users.noreply.github.com>
2022-09-25 20:20:21 +0530

    Update First repeating character.c

--- First repeating character.c
+++ First repeating character.c
@@ -1,17 +1,15 @@
 /*
 A string S is passed as the input. S has at least one repeating character. The program must print the first repeating character C.
 
-Input Format: The first line contains S.
+Input Format:  The first line contains S.
 
 Output Format: The first line contains C.
 
 Boundary Conditions: Length of S will be from 3 to 100.
 
-Example Input/Output 1: Input: abcdexyzbwqpoolj
-
-Output:
-
-b
+Example Input/Output 1: 
+Input:  abcdexyzbwqpoolj
+Output: b
 
 Solution:
 */
