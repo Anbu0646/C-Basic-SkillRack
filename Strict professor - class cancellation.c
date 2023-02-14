374e51f605730e6fbda9680882208546dc3664cc
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-14 20:45:50 +0530

    Create Strict professor - class cancellation.c

---
+++ Strict professor - class cancellation.c
@@ -0,0 +1,60 @@
+/*
+In a college, a professor is strict and mandates that at least M students out of the total N students must arrive on time to his class. Else he would cancel the class.
+The start time of the class is also passed as the input. The individual arrival time for N students is also passed as input in 24 hour format. 
+The program must print if the class was cancelled or not. 
+
+Input Format:  The first line will contain N and M separated by a space. 
+               The second line will contain the start time of the class. 
+               The third line will contain the arrival time of the N students separated by one or more space(s). 
+
+Output Format: First line will contain either Yes or No (Yes for cancellation of the class and No if the class is not cancelled). 
+
+Boundary Conditions: 3 <= N <= 50 
+2 <= M <= N 
+
+Example Input/Output 1: 
+
+Input:  5 3
+        9:30
+        9:30 9:38 9:31 9:32 9:31 
+
+Output: Yes
+
+Explanation: 4 out of the 5 students arrived late, which means only one arrived on time.
+             As the professor has mandated 3 out of the 5 must arrive on time, the class was cancelled and hence Yes is printed as the output.
+
+Example Input/Output 2:
+
+Input: 6 4 
+       11:10
+       10:30 10:40 10:55 11:20 11:10 11:12 
+
+Output: No
+
+SOLUTION:
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int tot, t, m, m1, h, h1, s, c=0;
+    char ch;
+    scanf("%d %d\n %d%c%d\n", &tot, &s, &t, &ch, &m);
+    
+    for(int i=0; i<tot; i++)
+    {
+        scanf("%d%c%d", &h1, &ch, &m1);
+        if(t>h1)
+        {
+            c+=1;
+        }
+        else if(t==h1 && m>=m1)
+        {
+            c+=1;
+        }
+    }
+    
+    (c>=s)?printf("No"):printf("Yes");
+}
