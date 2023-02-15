79ce6776ca2945588e8bfc010bef0ba06cde9fb4
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-15 14:41:17 +0530

    Create Next Number Palindrome.c

---
+++ Next Number Palindrome.c
@@ -0,0 +1,44 @@
+/*
+
+INPUT 1:
+909
+
+OUTPUT 1:
+919
+
+INPUT 2:
+121
+
+OUTPUT 2:
+131
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n, t, s;
+    scanf("%d", &n);
+    n+=1;
+    
+    while(1)
+    {
+        n++;
+        t=n; 
+        s=0;
+        
+        while(t>0)
+        {
+            s=(s*10)+(t%10);
+            t/=10;
+        }
+        
+        if(n==s)
+        {
+            printf("%d", n);
+            break;
+        }
+    }
+}
