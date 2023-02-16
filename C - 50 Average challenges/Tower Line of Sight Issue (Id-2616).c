85b32fbfcb56246054024d2acc57a7b172310118
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-16 20:52:04 +0530

    Update Tower Line of Sight Issue (Id-2616).c

--- C - 50 Average challenges/Tower Line of Sight Issue (Id-2616).c
+++ C - 50 Average challenges/Tower Line of Sight Issue (Id-2616).c
@@ -2,7 +2,7 @@
 Four towers A, B, C, D are to be erected. Tower A is to communicate with tower C. Tower B is to communicate with tower D. Line of sight issue can occur under the following conditions - when tower B or D is in the straight line connecting A and C - when tower A or C is in the straight line connecting B and D The program must accept the co-ordinates of all four towers and print yes or no depending on whether Line of sight issue will occur or not. 
 
 Input Format:  The first line will contain X and Y co-ordinates of tower A separated by a space. 
-		   The second line will contain X and Y co-ordinates of tower B separated by a space. 
+	       The second line will contain X and Y co-ordinates of tower B separated by a space. 
                The third line will contain X and Y co-ordinates of tower C separated by a space. 
                The fourth line will contain X and Y co-ordinates of tower D separated by a space 
 Output Format: The first line will contain yes or no (smaller case) 
@@ -11,12 +11,18 @@ Boundary Conditions: The value of the co-ordinates will be from -500 to 500.
 
 Example Input/Output 1: 
 
-Input:  0 0 0 -2 2 0 0 2 
+Input:  0 0 
+	0 -2
+	2 0
+	0 2 
 Output: yes 
 
 Example Input/Output 2: 
 
-Input:  0 0 0 -2 2 0 0 -5 
+Input:  0 0
+	0 -2 
+	2 0 
+	0 -5 
 Output: no
 
 
