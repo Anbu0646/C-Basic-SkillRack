d15e9c37eb5290e057b94815b86490059bb46d82
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-20 22:20:30 +0530

    Create Characters arrangement - A and B.py

---
+++ Characters arrangement - A and B.py
@@ -0,0 +1,25 @@
+'''
+The program must accept a string S (containing only A and B) and an integer K as the input.
+The program must print yes if the characters can be arranged in the string such that there are at least K number of B present between every two A. 
+
+Boundary Condition(s): 1 <= Length of S <= 1000,  1 <= K <= Length of S 
+
+Input Format:  The first line contains S. The second line contains K.
+Output Format: The first line contains yes or no. 
+
+Example Input/Output 1: 
+
+Input:  ABABBABBBB 
+        3 
+Output: yes 
+
+Explanation: The string ABABBABBBB can be arranged as ABBBABBBBA. Here there are at least three B present between all every A characters. 
+
+Example Input/Output 2: 
+
+Input:  ABBA 
+        3
+Output: no
+
+SOLUTION:
+'''
